0c496cf52b87bbfb7a3db5baf6b282fcf6b36684
IvanM <ivan.montes@outlook.com>
2024-01-17 18:56:32 +0100

    Correct msgs

--- src/main/java/dev/ime/solar_fleet/tool/MsgStatus.java
+++ src/main/java/dev/ime/solar_fleet/tool/MsgStatus.java
@@ -7,7 +7,7 @@ public final class MsgStatus {
 	public static final String INVALID_OBJECTID = "\"error\": \"Invalid ObjectId\"";
 	public static final String ENTITY_DELETED = "\"error\": \"Entity deleted\"";
 	public static final String ENTITY_NOT_DELETED = "\"error\": \"Entity NOT deleted\"";
-	public static final String USER_NOT_FOUND = "\"error\": \"User not found\"";
+	public static final String RESOURCE_NOT_FOUND = "\"error\": \"Resource not found\"";
 	public static final String EMPTY_LIST = "\"error\": \"Empty List -_-\"";
 	
 }
