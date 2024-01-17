69065ed9541039708738c8fe812db3abbb299a47
IvanM <ivan.montes@outlook.com>
2024-01-17 17:24:32 +0100

    Create file with messages constants

---
+++ src/main/java/dev/ime/solar_fleet/tool/MsgStatus.java
@@ -0,0 +1,13 @@
+package dev.ime.solar_fleet.tool;
+
+public final class MsgStatus {
+
+	private MsgStatus() {}
+	
+	public static final String INVALID_OBJECTID = "\"error\": \"Invalid ObjectId\"";
+	public static final String ENTITY_DELETED = "\"error\": \"Entity deleted\"";
+	public static final String ENTITY_NOT_DELETED = "\"error\": \"Entity NOT deleted\"";
+	public static final String USER_NOT_FOUND = "\"error\": \"User not found\"";
+	public static final String EMPTY_LIST = "\"error\": \"Empty List -_-\"";
+	
+}
