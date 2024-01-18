7a7e02a2cf9a2ba807fa72dfec20c2ccbf3e0f45
IvanM <ivan.montes@outlook.com>
2024-01-18 17:04:32 +0100

    Replace getAll by version with pagination query param

--- src/main/java/dev/ime/solar_fleet/resource/ShipClassResource.java
+++ src/main/java/dev/ime/solar_fleet/resource/ShipClassResource.java
@@ -1,5 +1,6 @@
 package dev.ime.solar_fleet.resource;
 
+
 import java.util.List;
 import java.util.Optional;
 
@@ -11,12 +12,17 @@ import dev.ime.solar_fleet.mapper.ShipClassMapper;
 import dev.ime.solar_fleet.service.ShipClassServiceImpl;
 import dev.ime.solar_fleet.tool.MsgStatus;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
 
 @Path("/api/shipclasses")
 public class ShipClassResource {
@@ -31,12 +37,19 @@ public class ShipClassResource {
 	}
 	
 	@GET
-	public Response getAll() {
+    @Produces(MediaType.APPLICATION_JSON)
+	public Response getAll(@QueryParam("page")String page) {
 		
-		List<ShipClass>list = shipClassServiceImpl.getAll();
+		List<ShipClass>list;
+		
+		if (page != null && page.matches("^[1-9]\\d*$") ) {
+			list = shipClassServiceImpl.getAllPaged(Integer.valueOf(page));
+        } else {
+        	list = shipClassServiceImpl.getAll();    		
+        }
+		
 		return list.isEmpty()? Response.ok(MsgStatus.EMPTY_LIST).build()
-								:Response.ok(shipClassMapper.toListShipClassDto(list)).build();
-		
+				:Response.ok(shipClassMapper.toListShipClassDto(list)).build();
 	}
 	
 	@GET
@@ -50,7 +63,7 @@ public class ShipClassResource {
 		Optional<ShipClass>opt = shipClassServiceImpl.getById(new ObjectId(id));
 		
 		return opt.isPresent()? Response.ok(shipClassMapper.toShipClassDto(opt.get())).build()
-								:Response.status(404).entity(MsgStatus.RESOURCE_NOT_FOUND).build();		
+								:Response.status(Response.Status.NOT_FOUND).entity(MsgStatus.RESOURCE_NOT_FOUND).build();		
 		
 	}
 	
@@ -67,7 +80,7 @@ public class ShipClassResource {
 	
 	@PUT
 	@Path("/{id}")
-	public Response update(@PathParam("id") String id,ShipClassDto dto) {
+	public Response update(@PathParam("id") String id, ShipClassDto dto) {
 		
 		if( !ObjectId.isValid(id) ){			
 			return Response.status(400).entity(MsgStatus.INVALID_OBJECTID).build();
@@ -76,9 +89,21 @@ public class ShipClassResource {
 		Optional<ShipClass>opt = shipClassServiceImpl.update(new ObjectId(id), shipClassMapper.toShipClass(dto));
 		
 		return opt.isPresent()? Response.ok(shipClassMapper.toShipClassDto(opt.get())).build()
-								:Response.status(404).entity(MsgStatus.RESOURCE_NOT_FOUND).build();	
+								:Response.status(Response.Status.NOT_FOUND).entity(MsgStatus.RESOURCE_NOT_FOUND).build();	
 		
 	}
 	
-	
+	@DELETE
+	@Path("/{id}")
+	public Response delete(@PathParam("id") String id) {
+
+		if( !ObjectId.isValid(id) ){			
+			return Response.status(400).entity(MsgStatus.INVALID_OBJECTID).build();
+		}
+		
+		return shipClassServiceImpl.delete(new ObjectId(id)) == 0? Response.ok(MsgStatus.ENTITY_DELETED).build()
+													:Response.status(Response.Status.NOT_FOUND)
+															.entity(MsgStatus.ENTITY_NOT_DELETED)
+															.build();		
+	}
 }
