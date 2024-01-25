d57a40469fc26264194b5e46f1965429a47c1a47
IvanM <ivan.montes@outlook.com>
2024-01-25 13:04:59 +0100

    Change regex pattern

--- src/main/java/dev/ime/solar_fleet/dto/spaceship/SpaceshipCreateDto.java
+++ src/main/java/dev/ime/solar_fleet/dto/spaceship/SpaceshipCreateDto.java
@@ -2,7 +2,12 @@ package dev.ime.solar_fleet.dto.spaceship;
 
 import org.bson.types.ObjectId;
 
+import dev.ime.solar_fleet.tool.RegexPattern;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
-public record SpaceshipCreateDto(String name, ObjectId shipClassId) {
+
+public record SpaceshipCreateDto(@Pattern( regexp = RegexPattern.NAME_FULL, message="{Pattern.Entity.name}")String name, 
+								@NotNull(message="{NotNull.Entity.id}")ObjectId shipClassId) {
 	 
 }
