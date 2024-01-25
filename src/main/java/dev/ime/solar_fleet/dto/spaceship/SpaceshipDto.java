252dbf04c337f5e8ea96a7c1adfc371677d1bcf7
IvanM <ivan.montes@outlook.com>
2024-01-25 13:04:23 +0100

    Add validations

--- src/main/java/dev/ime/solar_fleet/dto/spaceship/SpaceshipDto.java
+++ src/main/java/dev/ime/solar_fleet/dto/spaceship/SpaceshipDto.java
@@ -2,6 +2,12 @@ package dev.ime.solar_fleet.dto.spaceship;
 
 import org.bson.types.ObjectId;
 
-public record SpaceshipDto(ObjectId id, String name, ObjectId shipClassId) {
+import dev.ime.solar_fleet.tool.RegexPattern;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record SpaceshipDto(@NotNull(message="{NotNull.Entity.id}")ObjectId id, 
+							@Pattern( regexp = RegexPattern.NAME_FULL, message="{Pattern.Entity.name}")String name, 
+							@NotNull(message="{NotNull.Entity.id}")ObjectId shipClassId) {
 
 }
