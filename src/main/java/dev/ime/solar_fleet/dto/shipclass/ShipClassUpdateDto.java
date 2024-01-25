57ca18d367f0cfda637a9f19eafdc602c85f02fa
IvanM <ivan.montes@outlook.com>
2024-01-25 13:04:07 +0100

    Create DTO

---
+++ src/main/java/dev/ime/solar_fleet/dto/shipclass/ShipClassUpdateDto.java
@@ -0,0 +1,7 @@
+package dev.ime.solar_fleet.dto.shipclass;
+
+
+import dev.ime.solar_fleet.tool.RegexPattern;
+import jakarta.validation.constraints.Pattern;
+
+public record ShipClassUpdateDto(@Pattern( regexp = RegexPattern.NAME_FULL, message="{Pattern.Entity.name}")String name) {}
