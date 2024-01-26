8db642e4acf30da46a52504c96ff94ee4e164c09
IvanM <ivan.montes@outlook.com>
2024-01-26 16:55:56 +0100

    Change error by info prefix

--- src/main/java/dev/ime/solar_fleet/tool/MsgStatus.java
+++ src/main/java/dev/ime/solar_fleet/tool/MsgStatus.java
@@ -5,9 +5,9 @@ public final class MsgStatus {
 	private MsgStatus() {}
 	
 	public static final String INVALID_OBJECTID = "\"error\": \"Invalid ObjectId\"";
-	public static final String ENTITY_DELETED = "\"error\": \"Entity deleted\"";
+	public static final String ENTITY_DELETED = "\"info\": \"Entity deleted\"";
 	public static final String ENTITY_NOT_DELETED = "\"error\": \"Entity NOT deleted\"";
 	public static final String RESOURCE_NOT_FOUND = "\"error\": \"Resource not found\"";
-	public static final String EMPTY_LIST = "\"error\": \"Empty List -_-\"";
+	public static final String EMPTY_LIST = "\"info\": \"Empty List -_-\"";
 	
 }
