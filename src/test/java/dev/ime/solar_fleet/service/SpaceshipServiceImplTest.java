2346fca4aecc79cb03fdd190ca94fd1f54396edb
IvanM <ivan.montes@outlook.com>
2024-01-26 15:46:57 +0100

    Add rest of test cases

--- src/test/java/dev/ime/solar_fleet/service/SpaceshipServiceImplTest.java
+++ src/test/java/dev/ime/solar_fleet/service/SpaceshipServiceImplTest.java
@@ -32,10 +32,13 @@ class SpaceshipServiceImplTest {
 	
 	@Inject
 	private SpaceshipServiceImpl spaceshipServiceImpl;
+	
 	@InjectMock
 	private SpaceshipRepository spaceshipRepositoryMock;
+	
 	@InjectMock
 	private ShipClassRepository shipClassRepositoryMock;
+	
 	@InjectMock
 	private Checker checkerMock;
 	
@@ -44,12 +47,16 @@ class SpaceshipServiceImplTest {
 	private final String nameTest = "SSV Normandy";
 	private Spaceship spaceshipTest;
 	private ObjectId objectIdTest;
+	private ShipClass shipClassTest;
 	
 	@BeforeEach
 	private void createObjects() {
+		
 		spaceships = new ArrayList<>();
 		spaceshipTest = new Spaceship(new ObjectId(idTest),nameTest, new ObjectId(idTest));
 		objectIdTest = new ObjectId(idTest);
+		shipClassTest = new ShipClass(new ObjectId(idTest),nameTest);
+
 	}
 	
 	@Test
@@ -80,5 +87,144 @@ class SpaceshipServiceImplTest {
 		verify(spaceshipRepositoryMock,times(1)).findById(Mockito.any(ObjectId.class));
 	}
 	
+
+	@Test
+	void SpaceshipServiceImpl_getById_ReturnNull() {
+		
+		doReturn(null).when(spaceshipRepositoryMock).findById(Mockito.any(ObjectId.class));
+		
+		Optional<Spaceship> opt = spaceshipServiceImpl.getById(objectIdTest);
+				
+		assertAll(
+				()->Assertions.assertThat(opt).isNotNull(),
+				()->Assertions.assertThat(opt).isEmpty()
+			);	
+		verify(spaceshipRepositoryMock,times(1)).findById(Mockito.any(ObjectId.class));
+	}
 	
+	@Test
+	void SpaceshipServiceImpl_create_ReturnObject() {
+		
+		doReturn(true).when(checkerMock).checkObjectId(Mockito.any(ObjectId.class));
+		doReturn(shipClassTest).when(shipClassRepositoryMock).findById(Mockito.any(ObjectId.class));
+		doNothing().when(spaceshipRepositoryMock).persist(Mockito.any(Spaceship.class));
+
+		Optional<Spaceship> opt = spaceshipServiceImpl.create(spaceshipTest);
+		
+		assertAll(
+				()->Assertions.assertThat(opt).isNotNull(),
+				()->Assertions.assertThat(opt.get().getName()).isEqualTo(nameTest)
+			);	
+		verify(checkerMock,times(1)).checkObjectId(Mockito.any(ObjectId.class));
+		verify(shipClassRepositoryMock,times(1)).findById(Mockito.any(ObjectId.class));
+		verify(spaceshipRepositoryMock,times(1)).persist(Mockito.any(Spaceship.class));
+	}
+	
+
+	@Test
+	void SpaceshipServiceImpl_create_ReturnEmptyForBadShipClassId() {
+		
+		doReturn(false).when(checkerMock).checkObjectId(Mockito.any(ObjectId.class));
+
+		Optional<Spaceship> opt = spaceshipServiceImpl.create(spaceshipTest);
+		
+		assertAll(
+				()->Assertions.assertThat(opt).isNotNull(),
+				()->Assertions.assertThat(opt).isEmpty()
+			);	
+		verify(checkerMock,times(1)).checkObjectId(Mockito.any(ObjectId.class));
+	}
+	
+
+	@Test
+	void SpaceshipServiceImpl_create_ReturnEmptyForNotShipClassId() {
+		
+		doReturn(true).when(checkerMock).checkObjectId(Mockito.any(ObjectId.class));
+		doReturn(null).when(shipClassRepositoryMock).findById(Mockito.any(ObjectId.class));
+
+		Optional<Spaceship> opt = spaceshipServiceImpl.create(spaceshipTest);
+		
+		assertAll(
+				()->Assertions.assertThat(opt).isNotNull(),
+				()->Assertions.assertThat(opt).isEmpty()
+			);	
+		verify(checkerMock,times(1)).checkObjectId(Mockito.any(ObjectId.class));
+		verify(shipClassRepositoryMock,times(1)).findById(Mockito.any(ObjectId.class));
+	}
+	
+
+	@Test
+	void SpaceshipServiceImpl_update_ReturnObject() {
+		
+		doReturn(true).when(checkerMock).checkObjectId(Mockito.any(ObjectId.class));
+		doReturn(shipClassTest).when(shipClassRepositoryMock).findById(Mockito.any(ObjectId.class));
+		doReturn(spaceshipTest).when(spaceshipRepositoryMock).findById(Mockito.any(ObjectId.class));
+		doNothing().when(spaceshipRepositoryMock).persistOrUpdate(Mockito.any(Spaceship.class));
+
+		Optional<Spaceship> opt = spaceshipServiceImpl.update(objectIdTest, spaceshipTest);
+		
+		assertAll(
+				()->Assertions.assertThat(opt).isNotNull(),
+				()->Assertions.assertThat(opt.get().getName()).isEqualTo(nameTest)
+			);	
+		verify(checkerMock,times(2)).checkObjectId(Mockito.any(ObjectId.class));
+		verify(shipClassRepositoryMock,times(1)).findById(Mockito.any(ObjectId.class));	
+		verify(spaceshipRepositoryMock,times(1)).findById(Mockito.any(ObjectId.class));
+		verify(spaceshipRepositoryMock,times(1)).persistOrUpdate(Mockito.any(Spaceship.class));
+	}
+	
+	@Test
+	void SpaceshipServiceImpl_update_ReturnEmptyBadObjectId() {
+		
+		doReturn(false).when(checkerMock).checkObjectId(Mockito.any(ObjectId.class));
+		
+		Optional<Spaceship> opt = spaceshipServiceImpl.update(objectIdTest,spaceshipTest);
+
+		assertAll(
+				()->Assertions.assertThat(opt).isNotNull(),
+				()->Assertions.assertThat(opt).isEmpty()
+			);
+		verify(checkerMock,times(1)).checkObjectId(Mockito.any(ObjectId.class));
+	}
+	
+
+	@Test
+	void SpaceshipServiceImpl_update_ReturnEmptyForSpaceshipNotFound() {
+		
+		doReturn(true).when(checkerMock).checkObjectId(Mockito.any(ObjectId.class));
+		doReturn(shipClassTest).when(shipClassRepositoryMock).findById(Mockito.any(ObjectId.class));
+		doReturn(null).when(spaceshipRepositoryMock).findById(Mockito.any(ObjectId.class));
+
+		Optional<Spaceship> opt = spaceshipServiceImpl.update(objectIdTest, spaceshipTest);
+		
+		assertAll(
+				()->Assertions.assertThat(opt).isNotNull(),
+				()->Assertions.assertThat(opt).isEmpty()
+			);
+		verify(checkerMock,times(2)).checkObjectId(Mockito.any(ObjectId.class));
+		verify(shipClassRepositoryMock,times(1)).findById(Mockito.any(ObjectId.class));	
+		verify(spaceshipRepositoryMock,times(1)).findById(Mockito.any(ObjectId.class));
+	}
+	
+	@Test
+	void SpaceshipServiceImpl_delete_ReturnInt() {
+		
+		doReturn(true).when(spaceshipRepositoryMock).deleteById(Mockito.any(ObjectId.class));
+		
+		int returnValue = spaceshipServiceImpl.delete(objectIdTest);
+		
+		Assertions.assertThat(returnValue).isZero();
+		verify(spaceshipRepositoryMock,times(1)).deleteById(Mockito.any(ObjectId.class));
+	}
+	
+	@Test
+	void SpaceshipServiceImpl_delete_ReturnIntFalse() {
+		
+		doReturn(false).when(spaceshipRepositoryMock).deleteById(Mockito.any(ObjectId.class));
+
+		int returnValue = spaceshipServiceImpl.delete(objectIdTest);
+		
+		Assertions.assertThat(returnValue).isEqualTo(1);
+		verify(spaceshipRepositoryMock,times(1)).deleteById(Mockito.any(ObjectId.class));
+	}
 }
