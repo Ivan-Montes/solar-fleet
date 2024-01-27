d9657ebe5254ae8d1581e669fb39e890a797ac19
IvanM <ivan.montes@outlook.com>
2024-01-27 17:45:24 +0100

    Add two constants for delete errors

--- src/main/java/dev/ime/solar_fleet/tool/MsgStatus.java
+++ src/main/java/dev/ime/solar_fleet/tool/MsgStatus.java
@@ -9,5 +9,6 @@ public final class MsgStatus {
 	public static final String ENTITY_NOT_DELETED = "\"error\": \"Entity NOT deleted\"";
 	public static final String RESOURCE_NOT_FOUND = "\"error\": \"Resource not found\"";
 	public static final String EMPTY_LIST = "\"info\": \"Empty List -_-\"";
-	
+	public static final String ERROR_WTF = "\"error\": \"ERROR WTF\""; 
+	public static final String ERROR_DELETE_ASSOCIATED_ITEMS = "\"error\": \"Some items are still associated to this entity\""; 
 }
