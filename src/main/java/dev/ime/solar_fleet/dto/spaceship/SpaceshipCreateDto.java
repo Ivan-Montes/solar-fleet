d607443433c17a3de1008fa682162447f3e406bd
IvanM <ivan.montes@outlook.com>
2024-01-29 17:04:21 +0100

    Create Dto for Spaceship

--- src/main/java/dev/ime/solar_fleet/dto/spaceship/SpaceshipCreateDto.java
+++ src/main/java/dev/ime/solar_fleet/dto/spaceship/SpaceshipCreateDto.java
@@ -1,13 +1,13 @@
 package dev.ime.solar_fleet.dto.spaceship;
 
-import org.bson.types.ObjectId;
+
 
 import dev.ime.solar_fleet.tool.RegexPattern;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 
 
 public record SpaceshipCreateDto(@Pattern( regexp = RegexPattern.NAME_FULL, message="{Pattern.Entity.name}")String name, 
-								@NotNull(message="{NotNull.Entity.id}")ObjectId shipClassId) {
+								@NotEmpty(message="{NotEmpty.Entity.field]") @Pattern( regexp = RegexPattern.OBJECT_ID, message="{Pattern.Entity.objectId}")String shipClassId) {
 	 
 }
