fe053d97ec98df113b250f887a61e94195441c55
IvanM <ivan.montes@outlook.com>
2024-01-30 19:19:58 +0100

    Rename pattern

--- src/main/java/dev/ime/solar_fleet/tool/RegexPattern.java
+++ src/main/java/dev/ime/solar_fleet/tool/RegexPattern.java
@@ -10,7 +10,7 @@ public class RegexPattern {
 	public static final String NAME_BASIC = "[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s\\-\\.&,:]+";
 	public static final String NAME_FULL = "[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s\\-\\.&,:]{1,50}";
 	public static final String LASTNAME_BASIC = "[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s\\-\\.&,:]+";
-	public static final String LASTFULL = "[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s\\-\\.&,:]{1,50}";
+	public static final String LASTNAME_FULL = "[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s\\-\\.&,:]{1,50}";
 	public static final String PAGE = "^[1-9]\\d*$";
 	public static final String OBJECT_ID = "^[0-9a-fA-F]{24}$";
 	
