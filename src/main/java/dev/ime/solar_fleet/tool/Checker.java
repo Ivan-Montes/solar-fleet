0d24dbbbefa75e6f176d725c3addfbc0b209cedc
IvanM <ivan.montes@outlook.com>
2024-02-16 18:13:32 +0100

    Delete unused method

--- src/main/java/dev/ime/solar_fleet/tool/Checker.java
+++ src/main/java/dev/ime/solar_fleet/tool/Checker.java
@@ -1,19 +1,11 @@
 package dev.ime.solar_fleet.tool;
 
 
-import org.bson.types.ObjectId;
-
 import jakarta.enterprise.context.ApplicationScoped;
+
 
 @ApplicationScoped
 public class Checker {
-
-	
-	public boolean checkObjectId(ObjectId objectId) {
-		
-		return objectId != null && ObjectId.isValid(objectId.toString()); 
-	       
-	}
 	
 	public boolean checkPage(String page) {
 		
